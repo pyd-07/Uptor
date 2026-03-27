772968d0fada1fe9b3190bcfeadecf07022eb2f6
pyd-07 <piyush.24448@knit.ac.in>
2026-03-27 12:31:50 +0530

    Redirecting logged in users to dashboard

---
+++ apps/web/middleware.ts
@@ -0,0 +1,38 @@
+// middleware.ts
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+
+const publicRoutes = ["/about", "/login", "/register"]
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get('auth-token')?.value;
+  const { pathname } = request.nextUrl;
+
+  if (pathname === '/login') {
+    if (token) {
+      return NextResponse.redirect(new URL('/dashboard', request.url));
+    }
+    return NextResponse.next();
+  }
+
+  if (!token && !publicRoutes.includes(pathname)) {
+    return NextResponse.redirect(new URL('/about', request.url));
+  }
+
+  return NextResponse.next();
+}
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files like CSS/JS)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - Any file with an extension (e.g., .svg, .png, .jpg, .css)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)',
+  ],
+};
+
