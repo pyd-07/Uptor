2879cec223ed5935fbf3aa7ad177bb5b331df803
pyd-07 <piyush.24448@knit.ac.in>
2026-03-28 18:58:01 +0530

    fix: added workaround/default resend mail in case of no 'from' mail provided

--- utils/mailer.js
+++ utils/mailer.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY)
 async function sendEmail(to, subject, html) {
     try {
         const response = await resend.emails.send({
-            from: process.env.FROM_EMAIL,
+            from: process.env.FROM_EMAIL || "onboarding@resend.dev",
             to,
             subject,
             html
